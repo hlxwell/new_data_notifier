bbc185506e0ae5c7adec29bcbcf00291a53d780a
Michael He <hlxwell@gmail.com>
2011-03-17 13:56:49 +0800

    fix test

--- test/integration/new_data_notifier_test.rb
+++ test/integration/new_data_notifier_test.rb
@@ -4,7 +4,9 @@ class NewDataNotifierTest < ActiveSupport::IntegrationCase
   test "should send mail when there is new users" do
     assert Time.now >= NewDataNotifier::Notifier.get_last_sent_at
     assert NewDataNotifier::Notifier.get_latest_added_data.blank?
+    assert NewDataNotifier::Notifier.send_all_notification.blank?
     User.create :email => "a@a.com"
     assert !NewDataNotifier::Notifier.get_latest_added_data.blank?
+    assert !NewDataNotifier::Notifier.send_all_notification.blank?
   end
 end
